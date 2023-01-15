4066420216143adf755383ca99efa4ba24ae2a40
ThenesVara <114569016+ThenesVara@users.noreply.github.com>
2023-01-15 15:41:52 +0100

    Update Socket client.py

--- Test communication TCP IP/Tests sockets/Socket client.py
+++ Test communication TCP IP/Tests sockets/Socket client.py
@@ -1,15 +1,15 @@
+'''client envoi en TCP IP plusieurs valeurs au serveur'''
+
 import socket
 import time
 
 def client_program():
-    host = '172.31.0.1'  #'192.168.200.254' #socket.gethostname()  # as both code is running on same pc
-    print(host)
+    host = '172.31.0.1'  #socket.gethostname()  
     port = 5000  # socket server port number
-    print("etape 1")
+
     client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # instantiate
     client_socket.connect((host, port))  # connect to the server
-    print(client_socket)
-    print("etape 2")
+
     '''
     while client_socket.connect((host,port)) is not True:
         print(".")
@@ -18,12 +18,13 @@ def client_program():
     message = input(" -> ")  # take input
 
     while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
+        client_socket.send(message.encode())  # send 1 message
+        
+        #message from server
         data = client_socket.recv(1024).decode()  # receive response
-
         print('Received from server: ' + data)  # show in terminal
 
-        message = input(" -> ")  # again take input
+        message = input(" -> ")  # message suivant
 
     client_socket.close()  # close the connection
 
